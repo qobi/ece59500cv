2780b6c8a0b6b66f9261b0eabcac6529b3062d3b
Jeffrey Mark Siskind <qobi@purdue.edu>
2025-09-19 13:22:13 -0400

    *** empty log message ***

---
+++ distances.py
@@ -0,0 +1,152 @@
+import numpy as np
+from functools import reduce
+
+def L2_scalar(p1, p2):
+    return (p1-p2)*(p1-p2)
+
+def plus(x, y):
+    return x+y
+
+def L2_vector(distance):
+    def internal(p1, p2):
+        return reduce(plus,
+                      [distance(p1[i], p2[i]) for i in range(len(p1))],
+                      0)
+    return internal
+
+def dtw(distance):
+    def internal(s1, s2):
+        m = len(s1)
+        n = len(s2)
+        c = np.zeros((m, n))
+        c[0, 0] = distance(s1[0], s2[0])
+        for i in range(1, m):
+            c[i, 0] = distance(s1[i], s2[0])+c[i-1, 0]
+        for j in range(1, n):
+            c[0, j] = distance(s1[0], s2[j])+c[0, j-1]
+        for i in range(1, m):
+            for j in range(1, n):
+                c[i, j] = distance(s1[i], s2[j])+min(c[i-1, j],
+                                                     c[i, j-1],
+                                                     c[i-1, j-1])
+        return c[m-1, n-1]
+    return internal
+
+def dtw_with_image(distance):
+    def internal(s1, s2):
+        m = len(s1)
+        n = len(s2)
+        c = np.zeros((m, n))
+        b = [[False for j in range(n)] for i in range(m)]
+        image = np.ones((m, n))
+        c[0, 0] = distance(s1[0], s2[0])
+        for i in range(1, m):
+            c[i, 0] = distance(s1[i], s2[0])+c[i-1, 0]
+            b[i][0] = [i-1, 0]
+        for j in range(1, n):
+            c[0, j] = distance(s1[0], s2[j])+c[0, j-1]
+            b[0][j] = [0, j-1]
+        for i in range(1, m):
+            for j in range(1, n):
+                l = min(c[i-1, j], c[i, j-1], c[i-1, j-1])
+                c[i, j] = distance(s1[i], s2[j])+l
+                if l==c[i-1, j-1]:
+                    b[i][j] = [i-1, j-1]
+                elif l==c[i, j-1]:
+                    b[i][j] = [i, j-1]
+                elif l==c[i-1, j]:
+                    b[i][j] = [i-1, j]
+        i = m-1
+        j = n-1
+        while i>0 or j>0:
+            image[i, j] = 0
+            i, j = b[i][j]
+        image[i, j] = 0
+        return c[m-1, n-1], image
+    return internal
+
+infinity = float("inf")
+
+def hausdorf(distance):
+    def internal(points1, points2):
+        return reduce(max,
+                      [reduce(min,
+                              [distance(point1, point2)
+                                        for point2 in points2],
+                              infinity)
+                       for point1 in points1],
+                      -infinity)
+    return internal
+
+def chamfer(distance):
+    def internal(points1, points2):
+        return reduce(plus,
+                      [reduce(min,
+                              [distance(point1, point2)
+                                        for point2 in points2],
+                              infinity)
+                       for point1 in points1],
+                      0)
+    return internal
+
+def complete_linkage(distance):
+    def internal(points1, points2):
+        return reduce(max,
+                      [reduce(max,
+                              [distance(point1, point2)
+                                        for point2 in points2],
+                              -infinity)
+                       for point1 in points1],
+                      -infinity)
+    return internal
+
+def average_linkage(distance):
+    def internal(points1, points2):
+        return reduce(plus,
+                      [reduce(plus,
+                              [distance(point1, point2)
+                                        for point2 in points2],
+                              0)
+                       for point1 in points1],
+                      0)/(len(points2)*len(points2))
+    return internal
+
+def minimum_average(distance):
+    def internal(points1, points2):
+        return reduce(min,
+                      [reduce(plus,
+                              [distance(point1, point2)
+                                        for point2 in points2],
+                              0)
+                       for point1 in points1],
+                      infinity)
+    return internal
+
+def point_with_minimum_average_distance(distance):
+    def internal(points1, points2):
+        best = points1[0]
+        best_distance = reduce(plus,
+                               [distance(points1[0], point2)
+                                for point2 in points2],
+                               0)
+        for point1 in points1[1:]:
+            this_distance = reduce(plus,
+                                   [distance(point1, point2)
+                                    for point2 in points2],
+                                   0)
+            if this_distance<best_distance:
+                best = point1
+                best_distance = this_distance
+        return best
+    return internal
+
+def invert(distance):
+    def internal(point1, point2):
+        return distance(point2, point1)
+    return internal
+
+def bidirectional(distance, aggregate):
+    def internal(point1, point2):
+        return aggregate(distance(point1, point2),
+                         invert(distance)(point1, point2))
+    return internal
---
+++ nearest_neighbor_classifier_gui.py
@@ -0,0 +1,61 @@
+from gui import *
+from distances import *
+from nearest_neighbor_classifier import *
+
+points = []
+labels = []
+
+distance = L2_vector(L2_scalar)
+
+def clear_command():
+    global points, labels
+    points = []
+    labels = []
+    message("")
+    get_axes().clear()
+    redraw()
+
+def all_command():
+    resolution = 50
+    scale = 1.0/resolution
+    for y in range(resolution+1):
+        for x in range(resolution+1):
+            label = classify([scale*x, scale*y], distance, points, labels)
+            if label==0:
+                get_axes().plot([scale*x], [scale*y], "r.")
+            else:
+                get_axes().plot([scale*x], [scale*y], "b.")
+    redraw()
+
+def click(x, y):
+    message("")
+    if mode()==0:
+        points.append([x, y])
+        labels.append(mode())
+        get_axes().plot([x], [y], "r+")
+        redraw()
+    elif mode()==1:
+        points.append([x, y])
+        labels.append(mode())
+        get_axes().plot([x], [y], "b+")
+        redraw()
+    else:
+        if len(points)==0:
+            message("No data")
+        else:
+            label = classify([x, y], distance, points, labels)
+            if label==0:
+                message("Red")
+            elif label==1:
+                message("Blue")
+
+add_button(0, 0, "Clear", clear_command, nothing)
+mode = add_radio_button_group([[0, 1, "Red", 0],
+                               [0, 2, "Blue", 1],
+                               [0, 3, "Classify", 2]],
+                              lambda: False)
+add_button(0, 4, "All", all_command, nothing)
+add_button(0, 5, "Exit", done, nothing)
+message = add_message(1, 0, 6)
+add_click(click)
+start_fixed_size_matplotlib(7, 7, 2, 6)
